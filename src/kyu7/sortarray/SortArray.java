121152b08a9a93aec52b532f019b72de8489138a
Verdungo <q>
2020-04-15 13:59:43 +0500

    (7) Sort arrays-1

---
+++ src/kyu7/sortarray/SortArray.java
@@ -0,0 +1,35 @@
+package kyu7.sortarray;
+
+import java.util.Arrays;
+
+public class SortArray {
+    public static Object[] sortArray(Object[] names){
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static int[] sortArray(int[] names){
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static long[] sortArray(long[] names){
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static short[] sortArray(short[] names){
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static float[] sortArray(float[] names){
+        Arrays.sort(names);
+        return names;
+    }
+
+    public static double[] sortArray(double[] names){
+        Arrays.sort(names);
+        return names;
+    }
+}
